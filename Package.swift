93ed4440611efc09711e981043bd2a39a46ae9bd
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-09 19:41:43 +0530

    Updating Swift Package to 1.3.0

--- Package.swift
+++ Package.swift
@@ -3,9 +3,10 @@
 
 import PackageDescription
 
-// v1.2.0
+// v1.3.0
 let package = Package(
     name: "GoogleMobileAdsMediationFyber",
+    platforms: [.iOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -14,12 +15,10 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "InMobiSDK",
+            name: "GoogleMobileAdsMediationFyber",
             url: "https://github.com/AnyMindG/AllFrameworks/releases/download/10000.6.0/GoogleMobileAdsMediationFyber.zip",
             checksum: "c1582495b37211471d9f7f5142d07d6a732eab7be61e826c39c532fd4b13ff1a"
         ),
-        .target(
-            name: "GoogleMobileAdsMediationFyber"),
         .testTarget(
             name: "GoogleMobileAdsMediationFyberTests",
             dependencies: ["GoogleMobileAdsMediationFyber"]
